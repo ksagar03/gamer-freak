7a90f9ff272d74b1b48e107211f1de98aa3b1927
ksagar03 <sagar22krishna03@gmail.com>
2023-02-26 20:07:10 +0530

    setting up firebase for hosting this website

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB5NulY02NaTOlIkYBfoww6NqksaxnX-YE",
+  authDomain: "gamer-freak.firebaseapp.com",
+  projectId: "gamer-freak",
+  storageBucket: "gamer-freak.appspot.com",
+  messagingSenderId: "683449690775",
+  appId: "1:683449690775:web:ba9ea38040617924a3d7d2",
+  measurementId: "G-EH2R2XFSWB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
