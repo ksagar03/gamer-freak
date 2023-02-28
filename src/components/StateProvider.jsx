8a8fb6b357563eab44d7efa59cb1321166bb1bb4
ksagar03 <sagar22krishna03@gmail.com>
2023-02-28 22:43:03 +0530

    modified the context api logic in stateProvider

--- src/components/StateProvider.jsx
+++ src/components/StateProvider.jsx
@@ -12,4 +12,25 @@ export const StateProvider = ({ reducer, initialState, children }) => (
 /* here this "StateProvider" needs to be wraped with the components which requires data saved in the data layer, here I require this datalayer to be wraped up with all the components, so for this i will be wraping this "StateProvider" to App component which is rendered in index.js file, for more information check "index.js "file */
 
 // next to pull information from the data layer
-export  const useStateValue = () => useContext(StateContext)
+export  const useStateValue = () => {
+  const useStateval= useContext(StateContext) 
+    if (useStateval === undefined)
+    {
+      throw new Error("stateprovider is not wraped for the component please check")
+    }
+    return useStateval
+  }
+
+// Note: using or calling datalayer in all the components will cause rerendering of each component(data layer will be called in this component) whenever a data is added or removed.
+// if the datalayer is coverd only two components and user tries to access in 3rd component then that will throw a undefined state error 
+// ---- Eg:-> 
+// // export  const useStateValue = () => {
+//   const useStateval= useContext(StateContext) 
+//   if (useStateval === undefined)
+//   {
+//     throw new Error("stateprovider is not wraped for this components")
+//   }
+//   return useStateval
+
+// }
+
--- src/firebase.js
+++ src/firebase.js
@@ -18,4 +18,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// const db=app.firestore()
 const analytics = getAnalytics(app);
